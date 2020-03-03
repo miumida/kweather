69fd58c4f80bc5feac94b820cd958779f84bb92f
miumida <46196137+miumida@users.noreply.github.com>
2020-03-03 20:41:11 +0900

    Add files via upload

---
+++ custom_components/kweather/sensor.py
@@ -0,0 +1,160 @@
+"""Support for K-Weather Living Jisu Sensors."""
+import logging
+import requests
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+
+from datetime import timedelta
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (CONF_NAME, CONF_MONITORED_CONDITIONS)
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
+
+REQUIREMENTS = ['xmltodict==0.12.0']
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_AREA = 'area'
+
+# 지역별 url
+_JISU_URL = {
+    '01' : 'https://www.kweather.co.kr/data/JISU/11B00000.xml', #서울/경기
+    '02' : 'https://www.kweather.co.kr/data/JISU/11D10000.xml', #강원영서
+    '03' : 'https://www.kweather.co.kr/data/JISU/11D20000.xml', #강원영동
+    '04' : 'https://www.kweather.co.kr/data/JISU/11C10000.xml', #충청북도
+    '05' : 'https://www.kweather.co.kr/data/JISU/11C20000.xml', #충청남도
+    '06' : 'https://www.kweather.co.kr/data/JISU/11H10000.xml', #경상북도
+    '07' : 'https://www.kweather.co.kr/data/JISU/11H20000.xml', #경상남도
+    '08' : 'https://www.kweather.co.kr/data/JISU/11F10000.xml', #전라북도
+    '09' : 'https://www.kweather.co.kr/data/JISU/11F20000.xml', #전라남도
+    '10' : 'https://www.kweather.co.kr/data/JISU/11G00000.xml', #제주도
+}
+
+DEFAULT_NAME = 'kweather'
+
+MIN_TIME_BETWEEN_API_UPDATES = timedelta(seconds=30)
+MIN_TIME_BETWEEN_SENSOR_UPDATES = timedelta(seconds=3600)
+
+SCAN_INTERVAL = timedelta(seconds=7200)
+
+_INFORMATIONS = {
+    'picnic'     : [0,  '나들이', 'mdi:island'],
+    'laundry'    : [1,  '빨래',   'mdi:tumble-dryer'],
+    'carwash'    : [2,  '세차',   'mdi:car-wash'],
+    'fire'       : [4,  '불조심', 'mdi:fire'],
+    'exercise'   : [5,  '운동',   'mdi:weight-lifter'],
+
+    'pollution'  : [7,  '공해',   'mdi:emoticon-dead-outline'],
+    'corruption' : [12, '부패',   'mdi:emoticon-poop'],
+    'uv'         : [10, '자외선', 'mdi:weather-sunny-alert'],
+    'heating'    : [3,  '난방',   'mdi:hot-tub'],
+    'cold'       : [6,  '감기',   'mdi:thermometer-minus'],
+    'cooling'    : [8,  '냉방',   'mdi:air-filter'],
+    'feel'       : [9,  '불쾌',   'mdi:artist-outline'],
+}
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required(CONF_AREA): cv.string,
+    vol.Optional(CONF_MONITORED_CONDITIONS):
+        vol.All(cv.ensure_list, [vol.In(_INFORMATIONS)]),
+})
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up a Air Korea Sensors."""
+    name = config.get(CONF_NAME)
+    area = config.get(CONF_AREA)
+
+    informs = config.get(CONF_MONITORED_CONDITIONS)
+
+    sensors = []
+    if informs is not None:
+        real_time_api = KWeatherAPI(area)
+
+        for variable in informs:
+            sensors += [KWeatherSensor(name, variable, _INFORMATIONS[variable], real_time_api)]
+
+    add_entities(sensors, True)
+
+
+class KWeatherAPI:
+    """KWeather API."""
+
+    def __init__(self, area):
+        """Initialize the KWeather API.."""
+        self.area = area
+        self.result = {}
+
+    @Throttle(MIN_TIME_BETWEEN_API_UPDATES)
+    def update(self):
+        """Update function for updating api information."""
+        try:
+            url = _JISU_URL[self.area]
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            import xmltodict
+
+            self.result = xmltodict.parse(response.content.decode('utf8'))['jisu']['ndate'][0]['jtitle']
+        except Exception as ex:
+            _LOGGER.error('Failed to update KWeather API status Error: %s', ex)
+            raise
+
+
+class KWeatherSensor(Entity):
+    """Representation of a KWeather Sensor."""
+
+    def __init__(self, name, variable, variable_info, api):
+        """Initialize the KWeather sensor."""
+        self._name = name
+        self.var_id = variable
+        self.index    = variable_info[0]
+        self.var_name = variable_info[1]
+        self.var_icon = variable_info[2]
+
+        self.var_num   = None
+        self.var_jnum  = None
+        self.var_jtext = None
+
+        self.api = api
+        self.var_state = ''
+
+    @property
+    def entity_id(self):
+        """Return the entity ID."""
+        return 'sensor.kweather_{}'.format(self.var_id)
+
+    @property
+    def name(self):
+        """Return the name of the sensor, if any."""
+        return '{} 지수'.format(self.var_name)
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return self.var_icon
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self.var_state
+
+    @Throttle(MIN_TIME_BETWEEN_SENSOR_UPDATES)
+    def update(self):
+        """Get the latest state of the sensor."""
+        if self.api is None:
+            return
+
+        self.api.update()
+
+        self.var_state = self.api.result[self.index].get('jnum','-')
+        self.var_jnum  = self.api.result[self.index].get('jnum','-')
+        self.var_jtext = self.api.result[self.index].get('jtext','-')
+
+    @property
+    def device_state_attributes(self):
+        """Attributes."""
+        data = { 'jnum' : self.var_jnum,
+                 'jtext' : self.var_jtext }
+        return data
