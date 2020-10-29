e90d12323616f0f9924ab7d6812562895004243b
miumida <46196137+miumida@users.noreply.github.com>
2020-10-29 23:37:30 +0900

    Add files via upload

---
+++ custom_components/kweather/config_flow.py
@@ -0,0 +1,64 @@
+"""Config flow for naver_weather."""
+import logging
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.const import (CONF_SCAN_INTERVAL)
+
+from .const import DOMAIN
+
+CONF_AREA    = 'area'
+CONF_NAME    = 'name'
+
+default_area = '01'
+default_name = 'kweather'
+
+_LOGGER = logging.getLogger(__name__)
+
+class NaverWeatherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Naver Weather."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    def __init__(self):
+        """Initialize flow."""
+        self._area: Optional[str] = "01"
+        self._name: Optional[str] = "kweather"
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+
+        if user_input is not None:
+            self._area          = user_input[CONF_AREA]
+
+            return self.async_create_entry(title=DOMAIN, data=user_input)
+
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is None:
+            return self._show_user_form(errors)
+
+        #return self.async_create_entry(title=DOMAIN, data=user_input)
+
+    async def async_step_import(self, import_info):
+        """Handle import from config file."""
+        return await self.async_step_user(import_info)
+
+    @callback
+    def _show_user_form(self, errors=None):
+        default_area       = self._area
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_AREA, default=default_area): str,
+                vol.Optional(CONF_NAME, default=default_name): str,
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user", data_schema=schema, errors=errors or {}
+        )
---
+++ custom_components/kweather/const.py
@@ -0,0 +1,7 @@
+"""kweather constants."""
+DOMAIN   = "kweather"
+PLATFORM = "sensor"
+
+SW_VERSION = "1.1.0"
+MODEL   = "케이웨더 생활지수"
+MANUFAC = "Kweather"
